d57ddf808de123cda8e940386065175fd570ac0c
Sophia Thapa <sophia.thapa55@gmail.com>
2025-05-02 20:55:55 +0545

    add localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,13 @@
 import { products } from "./products.js";
 
-export let cart = [
-  { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 1 },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 4,
-  },
-  {
-    productId: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    quantity: 4,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+// if (!cart) {
+//   
+// }
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -28,6 +25,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -38,4 +36,5 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
-};
+  saveToStorage();
+}
